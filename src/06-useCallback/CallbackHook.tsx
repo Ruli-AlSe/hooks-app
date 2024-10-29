e1a160c32e0a3c6b593d1d56452eb5423ae5e4da
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-29 15:20:08 -0600

    Play with useCallback with arguments in case i need to send arguments to a funcion

--- src/06-useCallback/CallbackHook.tsx
+++ src/06-useCallback/CallbackHook.tsx
@@ -4,8 +4,10 @@ import { ShowIncrement } from './ShowIncrement';
 export const CallbackHook = () => {
   const [counter, setCounter] = useState(10);
 
-  const increment = useCallback(() => {
-    setCounter((value) => value + 1);
+  const increment = useCallback((incValue?: number) => {
+    const value = incValue ?? 1;
+
+    setCounter((c) => c + value);
   }, []);
 
   // const increment = () => setCounter(counter + 1);
--- src/06-useCallback/ShowIncrement.tsx
+++ src/06-useCallback/ShowIncrement.tsx
@@ -1,14 +1,14 @@
 import { memo } from 'react';
 
 interface Props {
-  increment: () => void;
+  increment: (value?: number) => void;
 }
 
 export const ShowIncrement = memo(({ increment }: Props) => {
   console.log('I was re-rendered...');
 
   return (
-    <button className="btn btn-primary" onClick={increment}>
+    <button className="btn btn-primary" onClick={() => increment(3)}>
       Increment
     </button>
   );
