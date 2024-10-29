43f6697c47eedb5282ad99fda76317e8a34dbde2
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-29 01:36:51 -0600

    Implement custom hook useFetch

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import { Pokemon } from '../interfaces/pokemon';
+
+interface FetchResponse {
+  data: Pokemon | null;
+  isLoading: boolean;
+  hasError: boolean | null;
+}
+interface FetchState extends FetchResponse {
+  error: { [key: string]: string | number } | null;
+}
+
+export const useFetch = (url: string): FetchResponse => {
+  const [state, setState] = useState<FetchState>({
+    data: null,
+    isLoading: true,
+    hasError: null,
+    error: null,
+  });
+
+  useEffect(() => {
+    getFetch();
+  }, [url]);
+
+  const setLoadingState = () => {
+    setState({
+      data: null,
+      isLoading: true,
+      hasError: false,
+      error: null,
+    });
+  };
+
+  const getFetch = async () => {
+    setLoadingState();
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      setState({
+        data: null,
+        isLoading: false,
+        hasError: true,
+        error: {
+          code: response.status,
+          message: response.statusText,
+        },
+      });
+
+      return;
+    }
+
+    const data = await response.json();
+
+    setState({
+      data,
+      isLoading: false,
+      hasError: false,
+      error: null,
+    });
+  };
+
+  return {
+    data: state.data,
+    isLoading: state.isLoading,
+    hasError: state.hasError,
+  };
+};
