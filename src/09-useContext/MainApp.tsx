3609462a2936e43cf9cbd8b10e06c6fdb762b52b
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-30 02:30:32 -0600

    Create a navbar component using bootstrap

--- src/09-useContext/MainApp.tsx
+++ src/09-useContext/MainApp.tsx
@@ -2,11 +2,14 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { AboutPage } from './AboutPage';
 import { HomePage } from './HomePage';
 import { LoginPage } from './LoginPage';
+import { Navbar } from '../components/Navbar';
 
 export const MainApp = () => {
   return (
     <>
       <h1>Main App</h1>
+
+      <Navbar />
       <hr />
 
       <Routes>
